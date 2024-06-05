3199acb71ff40aec9e3428e360b36b64c1f5a89c
MauricioGomez <mauricio.gomez.15@hotmail.com>
2024-06-04 21:13:06 -0300

    update mobile

--- src/components/Layout/LayoutStyles.jsx
+++ src/components/Layout/LayoutStyles.jsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const LayoutWrapper = styled.div`
   width: 100%;
-  height: auto;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  background-color: beige;
 `;
